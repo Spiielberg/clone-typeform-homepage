4c6be04015aae9104364f59dbabf4352b816a18a
Spiielberg <spiielberg@gmail.com>
2020-09-15 23:46:52 -0300

    Fim da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import Header from './components/Header/index';
+import FirstAndSecond from './components/FirstAndSecond';
+
+import GlobalStyles from './styles/global';
+import { Main, Section, Sticky } from './styles';
 
 function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <Header />
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className='third' />
+        </Section>
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className='fourth' />
+        </Section>
+      </Main>
+    </>
   );
 }
 
